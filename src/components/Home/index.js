86e0375cde89d21db796cfa940fbb6e28b4ce0c0
VeerabathinK <vkalyan.v19@gmail.com>
2023-08-02 08:25:21 +0000

    solution

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -12,26 +12,9 @@ import './index.css'
 const settings = {
   dots: true,
   infinite: true,
-  speed: 500,
+
   slidesToShow: 4,
   slidesToScroll: 4,
-
-  responsive: [
-    {
-      breakpoint: 1024,
-      settings: {
-        slidesToShow: 4,
-        slidesToScroll: 1,
-      },
-    },
-    {
-      breakpoint: 400,
-      settings: {
-        slidesToShow: 3,
-        slidesToScroll: 1,
-      },
-    },
-  ],
 }
 
 const renderOriginalsConstraints = {
